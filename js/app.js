6de6adfcf2340076546915e1ec24e32d08b9660f
Kyle Mahre <kfmahre@gmail.com>
2015-09-30 00:30:05 -0400

    lots of progress. Bugs and player in the game. Bugs moving in lanes randomly. Speed of bugs also random.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+var blockX = 101;
+var blockY = 83;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x , y) {
+    this.x = 0;
+    var randomLane = function() {
+    var lane = [60, 143, 226];
+        return lane[Math.floor(Math.random() * 3)];
+    };
+    this.y = randomLane();
+    this.speed = Math.floor(Math.random() * 15) + blockX;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +23,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > (blockX * 5)) {
+        this.x = -100;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +39,36 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x , y) {
+    this.x = 202;
+    this.y = 400;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction) {
+
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
 
+for (var i = 0; i <= 3; i++) {
+    var createEnemy = new Enemy();
+    allEnemies.push(createEnemy);
+}
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
